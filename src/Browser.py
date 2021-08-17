6bf5a6ab2aa9e935dcea085ba09004b12e96160f
Luccas Martins <luccasmartins.nave@gmail.com>
2021-08-17 11:08:40 -0300

    fix(browser): search accept cookies button by id

--- src/Browser.py
+++ src/Browser.py
@@ -44,7 +44,7 @@ class Browser:
             self.driver.get("https://www.ze.delivery/")
             time.sleep(3)
             try:
-                self.driver.find_element_by_class_name('accept-cookie-container').click()
+                self.driver.find_element_by_id('onetrust-accept-btn-handler').click()
                 time.sleep(1)
             except:
                 pass
